4b130fa430d1fe8df349c0720092f8decdf6583a
Adam Back <adb33@email.arizona.edu>
2014-12-09 22:15:32 -0800

    Added first function TP.

---
+++ reverse_string.py
@@ -0,0 +1,15 @@
+def reverse_string(word):
+  output = ''
+  letters = []
+  if type(word) is str:
+    for letter in word:
+      letters.append(letter)
+  
+    letters.reverse()
+
+    for letter in letters:
+      output += letter
+  
+  return output
+
+print reverse_string('Adam')
