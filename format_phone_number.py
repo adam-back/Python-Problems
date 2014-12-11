ab4bb6b5019b40a700365ed34f1e5dfd6c81ddb7
Adam Back <adb33@email.arizona.edu>
2014-12-11 12:11:26 -0800

    Created new tp.

---
+++ format_phone_number.py
@@ -0,0 +1,12 @@
+# Given a string which includes a user's phone number,
+# format the phone number as (123) 456-7890 and return it.
+# The number may be 7 or 10 digits.
+#
+# Example Test Cases (not all):
+# pretty_print_phone(1234567) //returns 123-4567
+# pretty_print_phone(1234567) //returns 123-4567
+# pretty_print_phone(1234567890) //returns (123) 456-7890
+# pretty_print_phone(123-456-7890) //returns (123) 456-7890
+
+
+def pretty_print_phone(userPhone):
